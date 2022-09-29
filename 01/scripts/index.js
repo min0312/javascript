7ad0d694aa704be7adf0a5bf9142a0781a5ad92e
pnumin <pnumin@pusan.ac.kr>
2022-09-29 17:42:00 +0900

    최종수정

--- 01/scripts/index.js
+++ 01/scripts/index.js
@@ -1,98 +1,56 @@
-// 기존 함수 선언 방식  
-/*
-function funPrint(n) {
-  console.log(n);
-  console.log("외부 자바스크립트") ; 
-  console.log("버튼"+1);
-}
-*/
+//랜덤수 
+let randNum ;
+//사용자가 선택한 수
+let userNum ;
 
-//ES6+ : 화살표 함수
-const funPrint = (n) => {
-  //console.log(n);
-  //console.log("외부 자바스크립트") ;
-  console.log(`버튼${n}`);
+//화면 보기 함수
+const dispDom = (msgP, inNumP, bt1P, bt2P) => {
+  //DOM 제어 
+  document.getElementById("msg").style.display = msgP;
+  document.getElementById("inNum").style.display = inNumP;
+  document.getElementById("bt1").style.display = bt1P;
+  document.getElementById("bt2").style.display = bt2P;
 }
 
-/*
-//변수 
-//console.log(x);
-let x = 100;
-x = 100 *2 ;
-console.log(x);
-*/
+//주사위
+//function showDice() {}
+const showDice = () => {
+  //랜덤수 생성
+  randNum = Math.floor(Math.random()*6) + 1 ;
+  console.log(`주사위 랜덤수 => ${randNum}`);
 
-/* jQuery 
-$document.ready() {
+  //DOM 제어
+  dispDom("none", "block", "none", "block"); 
 
 }
-*/
+
+//확인
+const showOk = () => {
+  userNum = form1.num.value ;
+
+  //DOM 제어 
+  dispDom("block", "none", "block", "none");  
+
+  //주사위 그림
+  let tag = `<img src="./images/${randNum}.png">` ;
+
+  //ox 그림
+  let ox;
+  if (randNum == userNum) ox = "o" ;
+  else ox = "x" ;
+
+  tag = `${tag}<img src="./images/${ox}.png">`;
+  
+  //이미지 표시
+  document.getElementById("msg").innerHTML = tag;
+  console.log(`사용자 선택 수 확인 => ${userNum}`);
+}
+
 
 //DOM이 생성이 되고 난 후에 요소를 CRUD해야한다.
-//document.addEventListener("DOMContentLoaded",콜백함수);
-//document.addEventListener("DOMContentLoaded",function () {});
-//document.addEventListener("DOMContentLoaded",() => {});
 document.addEventListener("DOMContentLoaded", () => {
-
-  //console.log(document.getElementById("bt").innerHTML);
-
-  /*요소 생성*/
-  //let bt = document.createElement("button")
-  //document.getElementById("bt").append(bt);
-
-  //요소 찾기
-  /*
-  //결과 HTMLCollection
-  const bts1 = document.getElementsByClassName("bt1");
-  console.log(bts1);
-
-  //결과 HTMLCollection
-  const bts2 = document.getElementsByTagName("button");
-  console.log(bts2);
-
-  //결과 Node
-  const bts3 = document.querySelector(".bt1");
-  console.log(bts3);
-  */
-  //결과 NodeList
-  const bts4 = document.querySelectorAll(".bt1");
-  //console.log(bts4.length);
- 
-  // console.log(bts4[0].innerHTML);
-  // console.log(bts4[0].innerText);
-  // console.log(bts4[1].textContent);
- 
-  /* 반복문 for*/
-  //결과 NodeList를 반복으로 가져오기
-  // for(let i=0; i < bts4.length ; i++) {
-  //   console.log(bts4[i]);
-  //   //bts4[i].innerHTML = `mybutton${i+1}`
-  //   //console.log(bts4[i].innerHTML);
-  // } 
-
-  /* 반복문 for in => 키만 가지고 옴*/
-  // for(let i in bts4) {
-  //   console.log(i)
-  // }
-
-  /* 반복문 foeach : 메소드형식 중간에 종료할 수 없음*/
-  // bts4.forEach((item)=>{
-  //   console.log(item.innerHTML);
-  // });
-
-  // bts4.forEach((item, idx)=>{
-  //   console.log(`${idx} : ${item.innerHTML}`);
-  // });
-  
-  /*반복문 for .. of*/
-  for(let item of bts4) {
-    console.log(item.innerHTML);
-  }
-
-  for(let [idx, item] of bts4.entries()) {
-    console.log(`${idx} : ${item.innerHTML}`);
-    break;
-  }
+  //주사위 버튼만 보여야 함
+  dispDom("none", "none", "block", "none"); 
 
 }); 
 
